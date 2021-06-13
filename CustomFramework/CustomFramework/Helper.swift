80b49665417677aa3a1eef966abeddf620bb0cf2
Vinayak <vinayak.koti@philips.com>
2021-06-13 14:41:52 +0530

    Added Framework

---
+++ CustomFramework/CustomFramework/Helper.swift
@@ -0,0 +1,17 @@
+//
+//  Helper.swift
+//  CustomFramework
+//
+//  Created by Vinayak Koti on 13/06/21.
+//
+
+import Foundation
+
+public class Helper {
+    
+    private init(){}
+    
+    public static func doSomething() {
+        print("Did Something")
+    }
+}
